5341985de6c2e9dedfdc4548b3f8dc22545d7158
JustLewis <l.marlow@outlook.com>
2021-07-13 15:35:38 +0100

    Modeling possible

--- RayMarchingFun/Assets/Scene2/RayMarchScene.cs
+++ RayMarchingFun/Assets/Scene2/RayMarchScene.cs
@@ -11,16 +11,35 @@ public class RayMarchScene : MonoBehaviour
     private Camera Cam;
 
     public ComputeShader CS;
-    public ComputeBuffer CB; //currently holds player data
     public int RaySteps = 250;
+
+    private Vector3 SunDir = Vector3.Normalize(new Vector3(0.8f, 0.4f, 0.42f));
+
+    //player view Variables
+    public float RotAngle = -7.0f;
+    private Vector3 PlayerInput = new Vector3(0f, .50f, 2.0f);
+    
+    private Vector3 CurrentLocation = new Vector3(0, 0.0f, -0.10f);
+    private float CurrentSmooth = -0.1f;//initial smoothness
+    private int CurrentShape = 0; //0 elipse 1 torus 2 cube
+    private Vector3 CurrentRadius = new Vector3(0.1f,0.1f,0.1f);
+
+    public float MouseSpeedMultiplier = 1.0f;
+    public float InputSpeedMultiplier = 1.0f;
+
+    private List<float> SphereData = new List<float>();
+    private List<float> CubeData = new List<float>();
+    private List<float> TorusData = new List<float>();
 
     private int CSMain;
 
-    //Game Variables
-    public float RotAngle = -7.0f;
-    private Vector3 PlayerInput = new Vector3(0f, .50f,2.0f);
-    private Vector3 SunDir = Vector3.Normalize(new Vector3(0.8f, 0.4f, 0.42f));
-    private Vector3 SecondBall = new Vector3(0, 0.0f, -0.10f);
+    private ComputeBuffer SphereBuffer;
+    private ComputeBuffer TorusBuffer;
+    private ComputeBuffer CubeBuffer;
+
+    private int SphereCount;
+    private int TorusCount;
+    private int CubeCount;
 
     private void Awake()
     {
@@ -44,8 +63,15 @@ public class RayMarchScene : MonoBehaviour
     {
         Cursor.lockState = CursorLockMode.Locked;
 
+        SphereBuffer = new ComputeBuffer(20, sizeof(float) * 7); //pos,3Dradius,smoothness
+        CubeBuffer = new ComputeBuffer(20, sizeof(float) * 7); //pos,3Dradius,smoothness
+        TorusBuffer = new ComputeBuffer(20, sizeof(float) * 7); //pos,3Dradius,smoothness
+
         CSMain = CS.FindKernel("CSMain");
         CS.SetTexture(CSMain, "MapTex", Map);
+        CS.SetBuffer(CSMain, "SphereData", SphereBuffer);
+        CS.SetBuffer(CSMain, "CubeData", CubeBuffer);
+        CS.SetBuffer(CSMain, "TorusData", TorusBuffer);
 
         //initial Uniforms
         CS.SetFloats("Resolution", new float[] { Screen.width, Screen.height });
@@ -59,37 +85,117 @@ public class RayMarchScene : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
         if (Input.GetKey(KeyCode.Mouse0))
         {
-            RotAngle += Input.GetAxisRaw("Mouse X") * Time.deltaTime * 10.0f;
+            CurrentSmooth += Input.GetAxisRaw("Mouse ScrollWheel");// * Time.deltaTime;
+            CurrentRadius.x += Input.GetAxisRaw("Mouse X") * Time.deltaTime * MouseSpeedMultiplier;
+            CurrentRadius.y += Input.GetAxisRaw("Mouse Y") * Time.deltaTime * MouseSpeedMultiplier;
+            CurrentRadius.z += Input.GetAxisRaw("Vertical") * Time.deltaTime * MouseSpeedMultiplier;
         }
-        if (Input.GetKey(KeyCode.Mouse1))
+        else if (Input.GetKey(KeyCode.Mouse1))
         {
-            SecondBall.x += Input.GetAxisRaw("Mouse X") * Time.deltaTime;
-            SecondBall.y += Input.GetAxisRaw("Mouse Y") * Time.deltaTime;
+            CurrentLocation.x += Input.GetAxisRaw("Mouse X") * Time.deltaTime * MouseSpeedMultiplier;
+            CurrentLocation.y += Input.GetAxisRaw("Mouse Y") * Time.deltaTime * MouseSpeedMultiplier;
+            CurrentLocation.z += Input.GetAxisRaw("Vertical") * Time.deltaTime * InputSpeedMultiplier;
         }
-        SecondBall.z += Input.GetAxisRaw("Mouse ScrollWheel");// * Time.deltaTime;
+        else //no mouse held. Allow movement
+        { 
+            RotAngle += Input.GetAxisRaw("Mouse X") * Time.deltaTime * 10.0f;
+            PlayerInput.x -= Input.GetAxisRaw("Horizontal") * Time.deltaTime;
+            PlayerInput.z -= Input.GetAxisRaw("Vertical") * Time.deltaTime;
+        }
 
-        PlayerInput.x -= Input.GetAxisRaw("Horizontal") * Time.deltaTime;
-        PlayerInput.z -= Input.GetAxisRaw("Vertical") * Time.deltaTime;
+        if (Input.GetKeyUp(KeyCode.Return) || Input.GetKeyUp(KeyCode.KeypadEnter))
+        {
+            SetBuffers();
+        }
+        if(Input.GetKeyUp(KeyCode.E))
+        {
+            CurrentShape++;
+            //only 3 shapes
+            if(CurrentShape == 3)
+            {
+                CurrentShape = 0;
+            }
+        }
 
         SetUniforms();
 
         CS.Dispatch(CSMain, Screen.width / 8, Screen.height / 8, 1);
     }
 
+    private void ResetUniforms()
+    {
+        CurrentLocation = new Vector3(0, 0.0f, -0.10f);
+        CurrentSmooth = -0.1f;//initial smoothness
+        CurrentShape = 0; //0 elipse 1 torus 2 cube
+        CurrentRadius = new Vector3(0.1f,0.1f,0.1f);
+    }
+
+
+    private void SetBuffers()
+    {
+        if(CurrentShape == 0)
+        {
+            SphereData.Add(CurrentLocation.x);
+            SphereData.Add(CurrentLocation.y);
+            SphereData.Add(CurrentLocation.z);
+            SphereData.Add(CurrentRadius.x);
+            SphereData.Add(CurrentRadius.y);
+            SphereData.Add(CurrentRadius.z);
+            SphereData.Add(CurrentSmooth);
+            SphereBuffer.SetData(SphereData.ToArray());
+            SphereCount = SphereData.Count / 7;
+        }
+        if (CurrentShape == 1)
+        {
+            TorusData.Add(CurrentLocation.x);
+            TorusData.Add(CurrentLocation.y);
+            TorusData.Add(CurrentLocation.z);
+            TorusData.Add(CurrentRadius.x);
+            TorusData.Add(CurrentRadius.y);
+            TorusData.Add(CurrentRadius.z);
+            TorusData.Add(CurrentSmooth);
+            TorusBuffer.SetData(TorusData.ToArray());
+            TorusCount = TorusData.Count / 7;
+        }
+        if (CurrentShape == 2)
+        {
+            CubeData.Add(CurrentLocation.x);
+            CubeData.Add(CurrentLocation.y);
+            CubeData.Add(CurrentLocation.z);
+            CubeData.Add(CurrentRadius.x);
+            CubeData.Add(CurrentRadius.y);
+            CubeData.Add(CurrentRadius.z);
+            CubeData.Add(CurrentSmooth);
+            CubeBuffer.SetData(CubeData.ToArray());
+            CubeCount = CubeData.Count / 7;
+        }
+        ResetUniforms();
+    }
+
     private void OnDisable()
     {
-        //CB.Dispose();
+        SphereBuffer.Dispose();
+        TorusBuffer.Dispose();
+        CubeBuffer.Dispose();
     }
 
     private void SetUniforms()
     {
         CS.SetFloat("Delta", Time.deltaTime);
         CS.SetFloats("PlayerInput", DataHandler.PackAndRetrieveData(PlayerInput));
-        CS.SetFloats("SecondBall", DataHandler.PackAndRetrieveData(SecondBall));
         CS.SetFloats("RotAngle", RotAngle);
+
+        CS.SetFloats("CurrentLocation", DataHandler.PackAndRetrieveData(CurrentLocation));
+        CS.SetFloats("CurrentRadius", DataHandler.PackAndRetrieveData(CurrentRadius));
+        CS.SetFloat("CurrentSmooth", CurrentSmooth);
+        CS.SetInt("CurrentShape", CurrentShape);
+
+        CS.SetInt("SphereCount", SphereCount);
+        CS.SetInt("CubeCount", CubeCount);
+        CS.SetInt("TorusCount", TorusCount);
+
         CS.SetFloats("Time", Time.time);
         CS.SetInt("MaxRaySteps", RaySteps);
     }
